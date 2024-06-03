aacbc5b9938725617cfdc73143a2daf9871b1c27
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-03 06:44:48 +0530

    Update ipl.py

--- MahakRobot/modules/ipl.py
+++ MahakRobot/modules/ipl.py
@@ -41,24 +41,18 @@ def fetch_matches(url):
 @app.on_message(filters.command(["live"], prefixes=["/", "!", "%", ",", ".", "@", "#"]))
 async def send_live_scores(_, message):
     chat_id = message.chat.id
-    await message.reply("Fetching live scores...")
-    
     scores = fetch_matches(url_live)
     await app.send_message(chat_id, scores)
 
 @app.on_message(filters.command(["recent"], prefixes=["/", "!", "%", ",", ".", "@", "#"]))
 async def send_recent_matches(_, message):
     chat_id = message.chat.id
-    await message.reply("Fetching recent matches...")
-    
     scores = fetch_matches(url_recent)
     await app.send_message(chat_id, scores)
 
 @app.on_message(filters.command(["upcoming"], prefixes=["/", "!", "%", ",", ".", "@", "#"]))
 async def send_upcoming_matches(_, message):
     chat_id = message.chat.id
-    await message.reply("Fetching upcoming matches...")
-    
     scores = fetch_matches(url_upcoming)
     await app.send_message(chat_id, scores)
 
