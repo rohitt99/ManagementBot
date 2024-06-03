ccc6c41df1a43f2cf6beb337a767314ab5344e65
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-04 02:22:02 +0530

    Update users.py

--- MahakRobot/modules/users.py
+++ MahakRobot/modules/users.py
@@ -8,7 +8,7 @@ import time
 import datetime
 
 @dev_plus
-@Mukesh.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
+@app.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
 async def broadcast_handler(bot: Mukesh, m: Message):
     broadcast_msg = m.reply_to_message
     broadcast_type = m.text.split("_")[1].strip().lower()  # Extracting broadcast type from the command
