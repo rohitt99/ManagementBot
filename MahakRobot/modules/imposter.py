0d6482e94183723dcdb0fbce4b204fc3a7fffee2
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-04 17:39:03 +0530

    Update imposter.py

--- MahakRobot/modules/imposter.py
+++ MahakRobot/modules/imposter.py
@@ -30,7 +30,7 @@ ROY = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
-            url=f"https://t.me/nykaaxbot?startgroup=true"),
+            url=f"https://t.me/mahakxbot?startgroup=true"),
         InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/God_Ravana")
     ],
 ]
