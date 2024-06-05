1dd306a821bafe61958d98514b922d6a190d9f09
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-06-05 23:13:30 +0530

    Update __init__.py

--- MahakRobot/database/__init__.py
+++ MahakRobot/database/__init__.py
@@ -1,10 +1,4 @@
 
-
-#MIT License
-#Copyright (c) 2023, ©NovaNetworks
-from .chatsdb import *
-from .usersdb import *
-
 from async_pymongo import AsyncClient
 
 from MahakRobot import MONGO_DB_URI
@@ -14,21 +8,3 @@ DBNAME = "MahakRobot"
 mongo = AsyncClient(MONGO_DB_URI)
 dbname = mongo[DBNAME]
 
-from typing import Callable
-
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import Message
-
-from MahakRobot import Aaru 
-
-
-def is_admins(func: Callable) -> Callable:
-    async def non_admin(c: Ravan, m: Message):
-        if m.from_user.id == OWNER:
-            return await func(c, m)
-
-        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
-        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-            return await func(c, m)
-
-    return non_admin
