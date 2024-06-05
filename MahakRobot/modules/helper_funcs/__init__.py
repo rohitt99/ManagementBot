2a11186564369fbb4bb6c0a07a99b97257cc7ca8
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-05 19:09:00 +0530

    Update __init__.py

--- MahakRobot/modules/helper_funcs/__init__.py
+++ MahakRobot/modules/helper_funcs/__init__.py
@@ -1,1 +1,25 @@
+
 """Helpers, also known as Utilities"""
+
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from MahakRobot import OWNER, pbot as app
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: app, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+
+from .inline import *
+from .read import *
