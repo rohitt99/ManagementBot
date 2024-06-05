38b4effd0eff4c99f5fd6ccbdf68c3a156cd4221
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-05 19:28:06 +0530

    Update __init__.py

--- MahakRobot/database/__init__.py
+++ MahakRobot/database/__init__.py
@@ -13,3 +13,22 @@ DBNAME = "MahakRobot"
 
 mongo = AsyncClient(MONGO_DB_URI)
 dbname = mongo[DBNAME]
+
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from MahakRobot import Aaru 
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: Ravan, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
